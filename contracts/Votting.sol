d14f87d7a6dac1159a7e338e2bcf57b0b633835a
coderofphcity <agantemboy@gmail.com>
2024-02-23 00:36:30 +0100

    hardhat config

---
+++ contracts/Votting.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+contract Votting {
+
+    error POLL_EXIST();
+
+    struct Poll {
+        string title;
+        uint256 id;
+        string description;
+        address[] candidates;
+        bool voted;
+    }
+
+    mapping(uint256 => Poll) polls;
+    mapping(address => bool) registered;
+    Poll[] pollArray;
+
+    function createPool(
+        string memory _title,
+        uint256 _id,
+        string memory _description
+    ) public {
+
+        if (pollExists(_id)) {
+            revert POLL_EXIST();
+        }
+
+        Poll storage poll = polls[_id];
+        poll.title = _title;
+        poll.id = _id;
+        poll.description = _description;
+    }
+
+    function candidatesToBeVoted(uint256 _id) public {
+        require(!registered[msg.sender], "already registered");
+        
+        if (!pollExists(_id)) {
+            revert POLL_EXIST();
+        }
+        Poll storage poll = polls[_id];
+        poll.candidates.push(msg.sender);
+        poll.voted = true;
+    }
+
+    function getPolls() public view returns (Poll[] memory) {
+        return pollArray;
+    }
+
+    function getSinglePoll(uint256 _id) public view returns (Poll memory) {
+        return polls[_id];
+    }
+
+    function pollExists(uint256 _id) public view returns (bool) {
+        return polls[_id].id == _id;
+    }
+}
